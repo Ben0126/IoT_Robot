7e2c990f242be2f0cf3b71d188202529186ffcd8
Ben Yeh <75458848+Ben0126@users.noreply.github.com>
2024-06-05 02:14:01 +0800

    add marker

--- auto_follow_line.py
+++ auto_follow_line.py
@@ -3,6 +3,41 @@ from robomaster import robot, vision
 import numpy as np
 import time
 
+
+class MarkerInfo:
+    def __init__(self, x, y, w, h, info):
+        self._x = x
+        self._y = y
+        self._w = w
+        self._h = h
+        self._info = info
+
+    @property
+    def pt1(self):
+        return int((self._x - self._w / 2) * 1280), int((self._y - self._h / 2) * 720)
+
+    @property
+    def pt2(self):
+        return int((self._x + self._w / 2) * 1280), int((self._y + self._h / 2) * 720)
+
+    @property
+    def center(self):
+        return int(self._x * 1280), int(self._y * 720)
+
+    @property
+    def text(self):
+        return self._info
+
+markers = []
+
+def on_detect_marker(marker_info):
+    number = len(marker_info)
+    markers.clear()
+    for i in range(0, number):
+        x, y, w, h, info = marker_info[i]
+        markers.append(MarkerInfo(x, y, w, h, info))
+        print("marker:{0} x:{1}, y:{2}, w:{3}, h:{4}".format(info, x, y, w, h))
+
 class PointInfo:
     def __init__(self, x, y, theta, c):
         self._x = x  # 点的x坐标
@@ -52,44 +87,62 @@ def main():
     ep_vision = ep_robot.vision
     ep_camera = ep_robot.camera
     ep_chassis = ep_robot.chassis
+    ep_gripper = ep_robot.gripper
 
     ep_camera.start_video_stream(display=False)
-    ep_vision.sub_detect_info(name="line", color="blue", callback=on_detect_line)
 
-    pid = PIDController(Kp=33, Ki=0, Kd=0.5)
+    ep_gripper.open(power=10)
+    time.sleep(2)
+    ep_gripper.pause()
+    time.sleep(1)
+    print("open")
+
+    pid = PIDController(Kp=26, Ki=0, Kd=2.5)
 
     try:
         while True:
+            ep_vision.sub_detect_info(name="marker", callback=on_detect_marker)
             img = ep_camera.read_cv2_image(strategy="newest", timeout=0.5)
-            frame_width = img.shape[1]  # 获取图像宽度
+            frame_width = img.shape[1]  # 獲取圖像寬度
 
+            for j in range(0, len(markers)):
+                cv2.rectangle(img, markers[j].pt1, markers[j].pt2, (255, 255, 255))
+                cv2.putText(img, markers[j].text, markers[j].center, cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255),
+                            3)
+
+            if len(markers) > 0:
+                ep_chassis.drive_speed(x=0, y=0, z=0, timeout=0.5)
+                time.sleep(1)
+                break
+
+            ep_vision.sub_detect_info(name="line", color="blue", callback=on_detect_line)
             if line_list:
-                # 取最后10个检测到的点的平均数值
+                # 取最後10個檢測到的點的平均數值
                 recent_points = line_list[-10:]
                 avg_x = int(np.mean([p.pt[0] for p in recent_points]))
                 avg_y = int(np.mean([p.pt[1] for p in recent_points]))
 
+                setpoint = frame_width // 2  # 設定點為圖象中心
 
-                setpoint = frame_width // 2  # 设定点为图像中心
-                print('current_point', avg_x)
-
-                # 计算PID控制量
+                # 計算PID控制量
                 control_signal = pid.compute(setpoint, avg_x)
                 control_signal /= 100
-                print('control_signal', control_signal)
 
-                ep_chassis.drive_speed(x=3.5, y=0, z=control_signal)
+                ep_chassis.drive_speed(x=0.5, y=0, z=control_signal)
 
-                # 在图像上绘制检测到的点
+                # 圖像上繪製檢測到的點
                 for point in line_list:
                     cv2.circle(img, point.pt, 3, point.color, -1)
 
-                # 在图像上绘制平均点
+                # 在圖像上繪製平均點
                 cv2.circle(img, (avg_x, avg_y), 5, (0, 255, 0), -1)
 
             cv2.imshow("Line", img)
+
+
             if cv2.waitKey(1) & 0xFF == ord(' '):
                 ep_chassis.drive_speed(x=0, y=0, z=0, timeout=0.5)
+                time.sleep(1)
                 break
 
     except Exception as e:
